b2fe3f398375355383b50ece5112489c8583d953
Julien Corioland <julien.corioland@live.fr>
2017-10-28 08:09:15 +0200

    fix

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function sendMessageToEventHub(){
   console.log("Requests count: " + requestCount + ", Success count: " + successCount + ", Errors count: " + errorCount);
 };
 
-setInterval(sendMessageToEventHub, 100);
+setInterval(sendMessageToEventHub, 1000);
 
 var server = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
